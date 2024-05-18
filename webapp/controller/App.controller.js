1164be27dae1f0c0f4c37f78a837f572cffa6f10
Julian Cantons <cantonsjulian@gmail.com>
2024-05-18 20:03:28 +0000

    First Commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("prueba.projectprueba.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"prueba/projectprueba/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
